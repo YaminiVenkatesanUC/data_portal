fa4ecd045e415df77aa6c61132644c757e1397b8
James McKay <james.mckay@stats.govt.nz>
2020-05-25 16:04:25 +1200

    fixed chorus load function

--- R/load_functions.R
+++ R/load_functions.R
@@ -267,14 +267,29 @@ read_trade_data <- function(config, directory) {
 
 chorus_load_function <- function(config, directory) {
   input_files <- list.files(paste0(directory, config$filename))
-  print(input_files)
   data <- foreach(i = 1:length(input_files), .combine=rbind) %do% {
-    as.data.frame(read_excel(
+    print(paste0(directory, config$filename, "/", input_files[[i]]))
+    output <- as.data.frame(read_excel(
       paste0(directory, config$filename, "/", input_files[[i]]),
       sheet = 1,
-      skip = 6,
+      skip = 4,
       col_names = c("date", "time", "egress", "ingress", "total")
     ), stringAsFactors = FALSE)
+    
+    if (!all(!is.na(output$date))) {
+      output <- output %>%
+        filter(
+          !(date %in% c("Grand Total"))
+        ) %>%
+        mutate(
+          date_parsed = ifelse(is.na(date), as.Date(as.numeric(date[[1]]), origin = "1899-12-30"), as.Date(as.numeric(date), origin = "1899-12-30"))
+        ) %>%
+        select(-c("date"))
+      
+      output$date <- as.Date(as.numeric(output$date_parsed),  origin = "1970-01-01")
+      output <- output %>% select(-c("date_parsed"))
+    }
+    output
   }
   data$hour <- substr(data$time, 1, 2)
   
