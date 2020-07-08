13487290bb37581251ff8d18c2be755fb1447220
RBaltaza <raymund.baltazar@stats.govt.nz>
2020-07-08 13:56:32 +1200

    cleaned up code

--- scripts/create_indicator_list.R
+++ scripts/create_indicator_list.R
@@ -1,49 +1,11 @@
-library(jsonlite)
-library(R6)
-library(httr)
-library(writexl)
-library(readxl)
-library(dplyr)
-library(tidyr)
+##note run load and shiny app before running this script
 library(xlsx)
+source("scripts/extract_indicator_list.R")
 
-
-source("R/core/build_ui.R")
-source("R/core/utils.R")
-source("R/core/download_modal.R")
-source("R/core/about_modal.R")
-source("R/core/main_panel.R")
-source("R/core/consts.R")
-source("R/core/get_download_csv.R")
-source("R/core/type_checks.R")
-source("R/core/data_interface.R")
-source("R/load_functions.R")
-source("R/plot_functions.R")
-source("R/data_types.R")
-source("R/data_service_functions.R")
-
-options(scipen = 999)
-
-CONFIG <- read_config_file()
-
-data_definitions <- read_json(CONFIG$data_definitions)
-
-
-
-# list_ind<-"/home/STATSNZ/rbaltaza/Network-Shares/U-Drive-SAS-03BAU/MEES/National Accounts/supply_ Use/R/collab/data_portal/"
-indicator_files<-"/home/STATSNZ/rbaltaza/Network-Shares/U-Drive-SAS-03BAU/MEES/National Accounts/supply_ Use/R/collab/data_portal/config/covid_19/"
-
-indicator_list<-read.csv(paste0(list_ind, "indicator_register.csv"), stringsAsFactors = F)
-
-by_class<-indicator_list%>%select(class, indicator_name, source)%>%mutate(number_of_charts=1)%>%group_by(indicator_name, class, source)%>%
+by_class<-output%>%select(class, indicator_name, source)%>%mutate(number_of_charts=1)%>%group_by(indicator_name, class, source)%>%
   summarise_if(is.numeric, sum, na.rm=T)%>%ungroup()%>%group_by(class, indicator_name, source)%>%as.data.frame()
 
-
-
-# data_definitions<-fromJSON(file= paste0(indicator_files,"covid_19_data_definitions.json"))
-
-
-
+data_definitions <- read_json(CONFIG$data_definitions)
 
 indicator_list<-function(data_definitions){
 
@@ -55,21 +17,17 @@ for (i in 1: length(data_definitions)){
     data<- t(df)
     
   }else{
-    
     df2<-data.frame(unlist(data_definitions[i]))
     df2<- t(df2)
     data<-merge(data, df2, all=T)
   }
-  
 }
 
 return(data)
 }
 
 indicator_data<-indicator_list(data_definitions)%>%select(class, type, indicator_name, filename) 
-
 list_indicators<-merge(indicator_data, by_class, all = T)%>%select(-number_of_charts)%>%as.data.frame()
-
 
 write.xlsx(list_indicators, "COVID - 19 data portal indicators.xlsx", sheetName = "Sheet1", row.names = FALSE)
 write.xlsx(by_class, "COVID - 19 data portal indicators.xlsx", sheetName = "Sheet2", append = TRUE, row.names = FALSE)
