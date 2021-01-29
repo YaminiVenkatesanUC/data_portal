04cf22ae8d1a4bf953599dc95383f5e35d6cd625
Vanessa Putnam <Vanessa.Putnam@stats.govt.nz>
2021-01-29 14:09:19 +1300

    added notes on implementation

--- scripts/API_testing.R
+++ scripts/API_testing.R
@@ -1,12 +1,11 @@
 CONFIG <- read_config_file()
 indicator <- read_json(CONFIG$indicator_definitions)[[1]]
 
-
 Resource <- GET(
   URLencode(paste0(CONFIG$odata_url,
                    "Covid-19Indicators/Resources",
                    "?$filter=(ResourceID eq '",
-                   indicator$api_resource_id,
+                   "",
                    "')")),
   add_headers("Ocp-Apim-Subscription-Key" = CONFIG$odata_token))  %>%
   content("text", encoding = "UTF-8") %>%
@@ -19,4 +18,4 @@ Resources_all <- GET(
   content("text", encoding = "UTF-8") %>%
   jsonlite::fromJSON(flatten = TRUE)
 
-View(select(Resources_all$value,ResourceID,Subject,Title)) # View resources and IDS
+View(select(Resources_all$value,ResourceID,Subject,Title,Var1,Var2,Var3)) # View resources and IDS
