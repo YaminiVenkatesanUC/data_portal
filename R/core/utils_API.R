f5a84e835270a788b52575896cef20632a172148
Vanessa Putnam <Vanessa.Putnam@stats.govt.nz>
2021-02-09 14:51:46 +1300

    added Notes and Caveat to json Resource

--- R/core/utils_API.R
+++ R/core/utils_API.R
@@ -3,16 +3,25 @@ data_frame_to_json_helper <- function(directory, config, data){
   print(config)
   print(head(data))
   resource <- to_resource(config)
+  observations <- to_observations(config, data)
+}
+
+to_observations <- functions(config, data){
+
+
 }
 
 to_resource <- function(config){
   Frequency <- check_null(config$frequency)
-  Var1 <- check_null(config$Var1)
-  Var2 <- check_null(config$Var2)
+  Var1 <- check_null(config$var1)
+  Var2 <- check_null(config$var2)
+  Var3 <- check_null(config$var3)
   Resource <- tibble("ResourceID" = config$api_resource_id,
                        "Subject" = config$type,
                        "Title" = config$indicator_name,
                        "Description" = NA,
+                       "Notes" = NA,
+                       "Caveats" = NA,
                        "Source" = NA,
                        "SourceURL" = NA,
                        "Modified" = NA,
