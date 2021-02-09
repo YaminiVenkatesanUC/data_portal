1e2b24325579773d7581f6cd8746e10185a63205
Vanessa Putnam <Vanessa.Putnam@stats.govt.nz>
2021-02-09 14:32:17 +1300

    added Resource json func

--- R/core/utils_API.R
+++ R/core/utils_API.R
@@ -2,4 +2,29 @@
 data_frame_to_json_helper <- function(directory, config, data){
   print(config)
   print(head(data))
+  resource <- to_resource(config)
+}
+
+to_resource <- function(config){
+  Frequency <- check_null(config$frequency)
+  Var1 <- check_null(config$Var1)
+  Var2 <- check_null(config$Var2)
+  Resource <- tibble("ResourceID" = config$api_resource_id,
+                       "Subject" = config$type,
+                       "Title" = config$indicator_name,
+                       "Description" = NA,
+                       "Source" = NA,
+                       "SourceURL" = NA,
+                       "Modified" = NA,
+                       "Frequency" = Frequency,
+                       "Var1" = Var1,
+                       "Var2" = Var2,
+                       "Var3" = Var3) %>% toJSON(na ="null")
+}
+
+check_null <- function(value){
+  if (is.null(value)){
+    return(NA)
+  }
+  return(value)
 }
