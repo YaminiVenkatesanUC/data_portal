28e3bb4e53a4d4e6794b58e21fdcdcd23c205eb2
Vanessa Putnam <Vanessa.Putnam@stats.govt.nz>
2021-03-04 09:10:34 +1300

    cleaned up load flag

--- R/load_functions.R
+++ R/load_functions.R
@@ -39,10 +39,10 @@ read_from_csv <- function(config, directory, odata_definitions=NULL) {
   }
 
   if(any(config$ResourceID %in% odata_definitions$ResourceID)){
+    metadata <- odata_definitions[which(odata_definitions$ResourceID == config$ResourceID),]
     print("Adding data to API")
     print(config$indicator_name)
     print(metadata)
-    metadata <- odata_definitions[which(odata_definitions$ResourceID == config$ResourceID),]
     data_frame_to_api_helper(directory, config, metadata, data)
     return(NULL)
   }
@@ -92,10 +92,10 @@ read_from_excel <- function(config, directory, odata_definitions=NULL) {
   }
 
   if(any(config$ResourceID %in% odata_definitions$ResourceID)){
+    metadata <- odata_definitions[which(odata_definitions$ResourceID == config$ResourceID),]
     print("Adding data to API")
     print(config$indicator_name)
     print(metadata)
-    metadata <- odata_definitions[which(odata_definitions$ResourceID == config$ResourceID),]
     data_frame_to_api_helper(directory, config, metadata, data)
     return(NULL)
   }
