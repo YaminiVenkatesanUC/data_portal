c4a949102f61978eaa8beba11ab082ea3992affe
Vanessa Putnam <Vanessa.Putnam@stats.govt.nz>
2021-03-05 16:50:50 +1300

    fixed bug in individual load indicator script

--- scripts/dev/load_individual_indicator.R
+++ scripts/dev/load_individual_indicator.R
@@ -6,8 +6,15 @@ indicator_name <- "Source of cases"
 directory <- CONFIG$data_directory
 odata_definitions <- fromJSON(CONFIG$odata_definitions)
 data_definitions <- fromJSON(CONFIG$data_definitions)
-config <- data_definitions[data_definitions$indicator_name == indicator_name,] %>% as.list()
+data_definitions_to_load <- read_json(CONFIG$data_definitions)
 
-func <- eval(parse(text=config$load_function))
-data <- func(indicator_definition, directory, odata_definitions)
 
+
+data <- load_functions[[config$load_function]](config, directory, odata_definitions)
+
+get_config <- function(config, indicator_name){
+  data_definitions <- read_json(config)
+  for(data_definition in data_definitions){
+    if(data_definition$indicator_name == indicator_name){return(data_definition)}
+  }
+}
