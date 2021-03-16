6ae32d782029b446e91d2091b5776c7b04829a3a
Maya Gorbatovsky <maya.gorbatovsky@stats.govt.nz>
2021-03-16 15:39:40 +1300

    report script

--- scripts/indicator_report_maya.R
+++ scripts/indicator_report_maya.R
@@ -1,12 +1,13 @@
 library(jsonlite)
 library(writexl)
 
-indicators <- fromJSON(txt = "config/covid_19/covid_19_indicators.json")
+#indicators <- fromJSON(txt = "config/covid_19/covid_19_indicators.json")
 definitions <- fromJSON(txt = "config/covid_19/covid_19_data_definitions.json")
 
 definitions <- definitions %>% select(class, type, indicator_name, group_names)
+#indicators <- indicators %>% select(class, type, indicator_name, groups)
 
-#'By ethnicity' break down?--------------------------------
+# By ethnicity break down?-------------------------------------------
 
 filter_ethnicity <- c("ethnic", "European", "Pacific", "Māori")
 
@@ -16,6 +17,12 @@ for (ethnicity in filter_ethnicity) {
 
 definitions$filter <- definitions$ethnic | definitions$European | definitions$Pacific | definitions$Māori
 
+definitions <- definitions %>%
+  select(class, type, indicator_name, filter) %>%
+  arrange(filter, class, type, indicator_name) %>%
+  unique()
+
+# Unnested group names-------------------------------------
 definitions <- unnest(data = definitions, cols = group_names)
 
 definitions <- definitions %>%
@@ -23,6 +30,18 @@ definitions <- definitions %>%
   arrange(filter, class, type, indicator_name, group_names) %>%
   unique()
 
+# Without group names---------------------------------------
+definitions <- definitions %>%
+  select(class, type, indicator_name) %>%
+  arrange(class, type, indicator_name) %>%
+  unique()
 
 #Write output-------------------------------------------
-write_xlsx(x = definitions, path = "example_data/COVID-19 report.xlsx")
+previous <- as.data.frame(read_excel(path = "example_data/COVID-19 report.xlsx"))
+compare <- full_join(definitions, previous)
+
+if (sum(is.na(compare)) > 0) {
+  print("Indicator list changed...")
+  print(compare[which(is.na(compare)), ])
+}
+write_xlsx(x = definitions, path = paste0("example_data/COVID-19 report.xlsx"))
