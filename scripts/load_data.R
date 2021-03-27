3ea664e2f8f67f21be661eefd08c4953dbdb92e0
Vanessa Putnam <Vanessa.Putnam@stats.govt.nz>
2021-03-27 16:54:36 +1300

    added CPFIN 3-6

--- scripts/load_data.R
+++ scripts/load_data.R
@@ -24,7 +24,6 @@ source("R/load_functions.R")
 add_to_data_service <- function(data_definition, odata_definitions, data_store, config) {
   check_data_definition(data_definition)
   data_definition <- expand_data_definition_group_names(data_definition)
-  print(data_definition)
   data <- load_functions[[data_definition$load_function]](data_definition, config$data_directory, odata_definitions)
   update_date <- as.Date(file.info(paste0(config$data_directory, data_definition$filename))$mtime)
 
