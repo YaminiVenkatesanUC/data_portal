0f967b5f6f470f583fe4e70df3f35fe41a3e4839
Maya Gorbatovsky <maya.gorbatovsky@stats.govt.nz>
2021-03-31 16:09:36 +1300

    Weekly trade: link updated Weekly traffic: script improved

--- scripts/dev/weekly_traffic.R
+++ scripts/dev/weekly_traffic.R
@@ -3,14 +3,14 @@ library(readxl)
 directory <- "~/Network-Shares/U-Drive-SAS-03BAU/MEES/National Accounts/COVID-19 data_Secure/COVID-19_dashboard"
 path <- paste0(directory, "/TSM")
 files <- file.info(list.files(path, full.names = TRUE))
-update <- read_excel(rownames(files)[which.max(files$mtime)], sheet = 1)
+update <- read_excel(rownames(files)[which.max(files$mtime)], sheet = "Sumated_Data")
 update$Day <- ymd(update$Day)
 
 regions <- unique(update$Region)
 
 for (region in regions) {
   data <- update %>%
-    select(-Weekday_Flag, -SiteName) %>%
+    select(Day, Type, Region, TrafficCount) %>%
     filter(Region == region) %>%
     select(-Region) %>%
     pivot_wider(names_from = Type, values_from = TrafficCount) %>%
@@ -21,3 +21,4 @@ for (region in regions) {
   writexl::write_xlsx(x = data, path = paste0(directory, "/COVID-19 - Weekly traffic count - ", region, ".xlsx"))
 
 }
+
