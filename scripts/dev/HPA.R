29af458230dd1c044c0dfebaf3668e7bbaca4081
yvenkate <yamini.venkatesan@stats.govt.nz>
2021-04-07 14:11:39 +1200

    HPA Drinking age added

--- scripts/dev/HPA.R
+++ scripts/dev/HPA.R
@@ -28,19 +28,62 @@ data <-
   read_excel(
     "~/Network-Shares/U-Drive-SAS-03BAU/MEES/National Accounts/COVID-19 data_Secure/COVID-19_dashboard/Raw data/Development/210310 HPA request selected tables Impact of COVID-19.xlsx",
     sheet = 2,
-    col_names = paste0("col_",2:11),
-    range = cell_limits(c(29,2),c(38,11))
-  ) %>% select(-col_3)
+    col_names = paste0("col_",1:10),
+    range = cell_limits(c(29,2),c(38,11)),
+    .name_repair = "minimal"
+  )
+
+names(data) <- c("Parameter","Total","18-24 W1","18-24 W2","25-49 W1","25-49 W2","50-65 W1","50-65 W2","65+ W1","65+ W2")
+data <- data %>% na.omit()
+data <- data %>% select(-Total) %>% filter(Parameter != "Total (Weighted)")
+
+data_long <- data %>% pivot_longer(data,cols = 2:ncol(data), names_to = "Age")
+
+data_long %>% separate(data_long, c("Age","Wave"), sep = " ")
 
 
 #names(data) <- c("Parameter","Total","Wave 1","Wave 2")
 
 data <- data %>% na.omit()
 
+# not_all_na <- function(x) any(!is.na(x))
+# data %>% select(where(not_all_na))
+#
+# data <- data[,colSums(is.na(data))<nrow(data)]
+
+
 #write_file_excel("Drinking by age", data)
 
 #openxlsx ::write.xlsx(data,"COVID 19 HPA data.xlsx", row.names = FALSE, sheetName = "Overall drinking")
 
+data <-
+  read_excel(
+    "~/Network-Shares/U-Drive-SAS-03BAU/MEES/National Accounts/COVID-19 data_Secure/COVID-19_dashboard/Raw data/Development/210310 HPA request selected tables Impact of COVID-19.xlsx",
+    sheet = 2,
+    col_names = paste0("col_",1:10),
+    range = cell_limits(c(29,2),c(38,11)),
+    .name_repair = "minimal"
+  )
+data <- as.data.frame(t(data))
+data <- data[colSums(!is.na(data)) > 0]
+
+data <- data[-2,]
+
+names(data) <- c("Age","Wave","Total","Less than you usually did before lockdown","About the same as you usually did before lockdown","More than you usually did before lockdown")
+
+data <- data[-1,] %>% select(-Total)
+
+data <- as.data.frame(data %>% pivot_longer(cols = 3:ncol(data),names_to = "Parameter"),stringAsFactors = FALSE)
+
+data <- as.data.frame(data %>% pivot_wider(names_from = Wave,values_from = value))
+
+data$`Wave 1` <- as.numeric(as.character(data$`Wave 1`))
+
+data$`Wave 2` <- as.numeric(as.character(data$`Wave 2`))
+
+
+
+openxlsx::write.xlsx(data,"COVID 19 HPA drinking age data.xlsx",row.names= FALSE)
 ## worrying about own drinking
 
 data <-
