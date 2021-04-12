b017e8df1c401e19a0fb6c204c635d3435240310
Maya Gorbatovsky <maya.gorbatovsky@stats.govt.nz>
2021-04-13 10:56:32 +1200

    report script

--- scripts/dev/reports/report.R
+++ scripts/dev/reports/report.R
@@ -10,5 +10,15 @@ definitions <- read_json(path = "config/covid_19/covid_19_data_definitions.json"
 #Number of indicators (single point)------------------------------------------------------------
 indNumber <- length(unique(indicators$indicator_name[indicators$disabled == FALSE | is.na(indicators$disabled)]))
 
-#Which indicators have been added this month?--------------------------------------------------
-added <- unique(definitions$indicator_name[definitions$])
+#Which indicators have been added / dropped this month?--------------------------------------------------
+added <- unique(definitions$indicator_name[dmy(definitions$date_added) > latest_report_date %m-% months(1)])
+dropped <- unique(definitions$indicator_name[!is.na(definitions$date_dropped) & dmy(definitions$date_dropped) > latest_report_date %m-% months(1)])
+
+#Number of indicators change over time
+monthly_count <- definitions %>%
+  filter(dmy(date_added) > dmy("31-07-2020")) %>%
+  mutate(reporting_period = ceiling_date(date_added, unit = "month")) %>%
+  select(indicator_name, reporting_period) %>%
+  unique() %>%
+  group_by(reporting_period) %>%
+  mutate(count = n())
