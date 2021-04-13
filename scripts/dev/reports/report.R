6212db6012af1a431a81f99bc7d67416e99e6a96
Maya Gorbatovsky <maya.gorbatovsky@stats.govt.nz>
2021-04-14 09:02:39 +1200

    report script

--- scripts/dev/reports/report.R
+++ scripts/dev/reports/report.R
@@ -1,5 +1,7 @@
 library(lubridate)
 library(jsonlite)
+library(dplyr)
+library(tidyr)
 
 current_date <- today()
 latest_report_date <- floor_date(rollback(current_date), unit = "month")
@@ -26,6 +28,7 @@ monthly_added <- definitions %>%
   mutate(count = n())
 
 #Total number of indicators each month------------------------------------------
+#add dots for indicators added that particular month!!!
 monthly_total <- definitions %>%
   mutate(reporting_period = floor_date(date_added, unit = "month")) %>%
   select(indicator_name, reporting_period) %>%
@@ -59,7 +62,27 @@ latest_views <- monthly_views$count[monthly_views$reporting_period == latest_rep
 previous_views <- monthly_views$count[monthly_views$reporting_period == latest_report_date %m-% months(1)]
 perc_change <- round(latest_views / previous_views * 100 - 100, 1)
 
-#Most visited indicators this month
+#Most visited indicators this month----------------------------------------------------
 #download Unique page views over time 01-03-2020 to 31-03-2021 // Google analytics
+#visualize as histogram +++ list???
 report_month <- read.csv("scripts/dev/reports/COVID-19 Data portal analytics_Stats NZ web page dashboard_Table.csv")
+report_month <- report_month %>%
+  separate(col = Indicator, into = c("class", "type", "indicator_name", "group"), sep = "_") %>%
+  select(indicator_name, Total.Events) %>%
+  group_by(indicator_name) %>%
+  mutate(count = sum(Total.Events)) %>%
+  select(-Total.Events) %>%
+  unique()
+top_visited <- report_month %>%
+  arrange(desc(Total.Events)) %>%
+  head(n = 10)
+
+#Indicators not visited this month at all----------------------------------------------
+not_visited <- definitions %>%
+  select(class, type, indicator_name) %>%
+  left_join(report_month) %>%
+  filter(is.na(count)) %>%
+  unique()
+
+
 
