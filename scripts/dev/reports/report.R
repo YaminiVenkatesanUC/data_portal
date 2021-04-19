965e6820f1276ceffbe193884a2f7132fbc72335
Maya Gorbatovsky <maya.gorbatovsky@stats.govt.nz>
2021-04-20 11:04:54 +1200

    report script

--- scripts/dev/reports/report.R
+++ scripts/dev/reports/report.R
@@ -16,7 +16,7 @@ views_total$Date <- as.Date(views_total$Date, format = "%b %d, %Y")
 views_monthly_folder <- "~/Network-Shares/J-Drive-WLG-Shared/Indicators_aotearoa/Maya/reports/ind_views"
 downloads_monthly_folder <- "~/Network-Shares/J-Drive-WLG-Shared/Indicators_aotearoa/Maya/reports/downloads"
 
-df <- definitions %>%
+df_indicators <- definitions %>%
   left_join(indicators, by = c("class", "indicator_name", "type")) %>%
   select(indicator_name, group_names, filename, date_added, date_dropped, source, download, frequency, disabled) %>%
   filter(disabled == FALSE | is.na(disabled)) %>%
@@ -25,10 +25,20 @@ df <- definitions %>%
   filter(!is.na(date_added)) %>%
   mutate(month = floor_date(date_added, unit = "month"))
 
+series_vec <- c("region", "gender", "age", "ethnic", "sex", "industry")
+for (vec in series_vec) {
+  df_indicators[[vec]] <- str_detect(df_indicators$indicator_name, vec) | str_detect(df_indicators$group_names, vec)
+}
+df_indicators$gender <- df_indicators$gender | df_indicators$sex
+df_indicators <- select(df_indicators, - sex)
+df_indicators$other <- df_indicators$group_names != "undefined_name" & !(df_indicators$region | df_indicators$gender | df_indicators$age | df_indicators$ethnic | df_indicators$industry)
+
+df_indicators$source_internal <- str_detect(pattern = "Stats", string = df_indicators$source)
+
 
 #Monthly cumulative count of indicators number------------------------------------------
 #add dots for indicators added that particular month!!!
-ind_count <- df %>%
+ind_count <- df_indicators %>%
   group_by(month) %>%
   mutate(ind_count = n()) %>%
   select(month, ind_count) %>%
@@ -45,42 +55,50 @@ views_count <- views_total %>%
   arrange(month) %>%
   within(views_count_cum <- cumsum(views_count))
 
-df <- df %>%
-  left_join(ind_count) %>%
-  left_join(views_count)
+  ### get download reports for each month starting from March
+downloads_count <- data.frame("month" = c(), "download_count" = c())
+for (file in rownames(file.info(list.files(downloads_monthly_folder, full.names = T)))) {
+  print(file)
+  month <- str_extract(file, "\\d\\d\\d\\d-\\d\\d-\\d\\d")
+  print(month)
+  downloads <- as.data.frame(read.csv(file)) %>%
+    filter(Event.Category == "Data Downloaded") %>%
+    mutate(month = ymd(month)) %>%
+    select(month, Total.Events)
+  print(str(downloads))
+  names(downloads) <- c("month", "download_count")
+  print(str(downloads))
+  downloads_count <- rbind(downloads_count, downloads)
+}
+
+df_events <- ind_count %>%
+  left_join(views_count) %>%
+  left_join(downloads_count)
+
 
 
   ### get monthly views per each indicator
 
+df_ind_views <- tibble("indicator_name" = c(), "ind_views" = c(), "month" = c())
+
   for (file in rownames(file.info(list.files(views_monthly_folder, full.names = T)))) {
-    print(file)
     month <- str_extract(file, "\\d\\d\\d\\d-\\d\\d-\\d\\d")
     views <- as.data.frame(read.csv(file)) %>%
       separate(col = Indicator, into = c("class", "type", "indicator_name", "group"), sep = "_") %>%
       select(indicator_name, Total.Events) %>%
       group_by(indicator_name) %>%
       mutate(ind_views = sum(Total.Events)) %>%
+      mutate(month = ymd(month)) %>%
       select(-Total.Events) %>%
+      ungroup() %>%
       unique()
-    names(views)[length(names(views))] <- month
-    df <- df %>%
-      left_join(views, by = c("indicator_name"))
+    #names(views)[length(names(views))] <- month
+   df_ind_views <- rbind(df_ind_views, views)
   }
-
-df <- df %>%
-  pivot_longer(cols = starts_with("ind_views"), names_to = "names", values_to = "values")
-
-downloads_count <-
-### get download reports for each month starting from March
-
-
-
-df <- left_join(df, monthly_total)
-
 
 
 #Number of currently available indicators:
-indNumber <- df %>%
+indNumber <- df_indicators %>%
   select(indicator_name) %>%
   unique() %>%
   nrow()
