0abed14b94d77f072c672a69b54c7c889c64a54e
Maya Gorbatovsky <maya.gorbatovsky@stats.govt.nz>
2021-04-21 10:38:28 +1200

    report dashboard, app

--- scripts/dev/reports/report.R
+++ scripts/dev/reports/report.R
@@ -2,6 +2,7 @@ library(lubridate)
 library(jsonlite)
 library(dplyr)
 library(tidyr)
+library(stringr)
 
 current_date <- today()
 latest_report_date <- floor_date(rollback(current_date), unit = "month")
@@ -32,6 +33,7 @@ for (vec in series_vec) {
 df_indicators$gender <- df_indicators$gender | df_indicators$sex
 df_indicators <- select(df_indicators, - sex)
 df_indicators$other <- df_indicators$group_names != "undefined_name" & !(df_indicators$region | df_indicators$gender | df_indicators$age | df_indicators$ethnic | df_indicators$industry)
+df_indicators$no_subseries <- !(df_indicators$region | df_indicators$gender | df_indicators$age | df_indicators$ethnic | df_indicators$industry | df_indicators$other)
 
 df_indicators$source_internal <- str_detect(pattern = "Stats", string = df_indicators$source)
 
@@ -39,40 +41,47 @@ df_indicators$source_internal <- str_detect(pattern = "Stats", string = df_indic
 #Monthly cumulative count of indicators number------------------------------------------
 #add dots for indicators added that particular month!!!
 ind_count <- df_indicators %>%
-  group_by(month) %>%
-  mutate(ind_count = n()) %>%
-  select(month, ind_count) %>%
+  select(month, indicator_name) %>%
   unique() %>%
-  arrange(month) %>%
-  within(ind_count_cum <- cumsum(ind_count))
+  group_by(month) %>%
+  mutate(new_inds = n()) %>%
+  select(month, new_inds) %>%
+  unique()
 
 views_count <- views_total %>%
   mutate(month = floor_date(Date, unit = "month")) %>%
   group_by(month) %>%
   mutate(views_count = sum(Unique.Page.Views)) %>%
   select(-Unique.Page.Views, -Date) %>%
-  unique() %>%
-  arrange(month) %>%
-  within(views_count_cum <- cumsum(views_count))
+  unique()
 
   ### get download reports for each month starting from March
 downloads_count <- data.frame("month" = c(), "download_count" = c())
 for (file in rownames(file.info(list.files(downloads_monthly_folder, full.names = T)))) {
-  print(file)
   month <- str_extract(file, "\\d\\d\\d\\d-\\d\\d-\\d\\d")
-  print(month)
   downloads <- as.data.frame(read.csv(file)) %>%
     filter(Event.Category == "Data Downloaded") %>%
     mutate(month = ymd(month)) %>%
     select(month, Total.Events)
-  print(str(downloads))
   names(downloads) <- c("month", "download_count")
-  print(str(downloads))
   downloads_count <- rbind(downloads_count, downloads)
 }
 
-df_events <- ind_count %>%
-  left_join(views_count) %>%
+df_events <- data.frame(month = seq.Date(from = as.Date("2020-03-01"), to = latest_report_date, by = "month")) %>%
+  left_join(ind_count)
+df_events$new_inds <- ifelse(is.na(df_events$new_inds), 0, df_events$new_inds)
+df_events <- df_events %>%
+  arrange(month) %>%
+  within(ind_count_cum <- cumsum(new_inds))
+
+df_events <- df_events %>%
+  left_join(views_count)
+df_events$views_count <- ifelse(is.na(df_events$views_count), 0, df_events$views_count)
+df_events <- df_events %>%
+  arrange(month) %>%
+  within(views_count_cum <- cumsum(views_count))
+
+df_events <- df_events %>%
   left_join(downloads_count)
 
 
@@ -92,10 +101,8 @@ df_ind_views <- tibble("indicator_name" = c(), "ind_views" = c(), "month" = c())
       select(-Total.Events) %>%
       ungroup() %>%
       unique()
-    #names(views)[length(names(views))] <- month
    df_ind_views <- rbind(df_ind_views, views)
   }
-
 
 #Number of currently available indicators:
 indNumber <- df_indicators %>%
@@ -104,7 +111,7 @@ indNumber <- df_indicators %>%
   nrow()
 
 #Which indicators have been added / dropped this month?--------------------------------------------------
-added <- unique(df$indicator_name[df$date_added >= latest_report_date])
-dropped <- unique(df$indicator_name[!is.na(df$date_dropped) & dmy(df$date_dropped) >= latest_report_date])
+added <- unique(df_indicators$indicator_name[df_indicators$date_added >= latest_report_date])
+dropped <- unique(df_indicators$indicator_name[!is.na(df_indicators$date_dropped) & dmy(df_indicators$date_dropped) >= latest_report_date])
 
 
