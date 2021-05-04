d42de6d48ca3f558f7202b753ef4baa662e5e910
Vanessa Putnam <Vanessa.Putnam@stats.govt.nz>
2021-05-05 09:26:41 +1200

    Revert "adding odata config to new loads"
    This reverts commit b8e87c1cfd62a55e1095118dea6336467dd29c06.

--- R/load_functions.R
+++ R/load_functions.R
@@ -1288,7 +1288,7 @@ read_hpa_drinking_data <- function(config,directory,odata_definitions=NULL) {
   return(output_group)
 }
 
-read_vaccination <- function(config, directory, odata_definitions=NULL) {
+read_vaccination <- function(config, directory) {
   data_object <- read_from_excel(config, directory)
   group_name <- config$group_names[[1]]
   data <- as.data.frame(read_excel(
