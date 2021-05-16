3203e916b88b42ab3ff6b59e45ac2d27c67de16e
Maya Gorbatovsky <maya.gorbatovsky@stats.govt.nz>
2021-05-17 09:08:20 +1200

    scripts: added missing indicator names daily, weekly, monthly lists of scripts

---
+++ scripts/dev/__weekly.R
@@ -0,0 +1,28 @@
+# MONDAY ----------------------------------------------------------
+# Broadband usage by region
+source("scripts/dev/chorus_regional.R")
+
+# Weekly deaths: By region, By age and sex, By age
+source("scripts/dev/weekly_death.R")
+
+
+# WEDNESDAY -------------------------------------------------------
+# COVID-19 vaccines administered – cumulative +++ daily total
+source("scripts/dev/vaccination.R")
+
+# Exports by country (values) +++ Imports by country (values) +++ Imports by commodity (values) +++ Exports by commodity (values)
+source("scripts/dev/trade_data.R")
+
+# Weekly traffic count
+source("scripts/dev/weekly_traffic.R")
+
+
+# THURSDAY ---------------------------------------------------------
+# Weekly filled jobs (34 days) +++ Weekly median earnings (34 days)
+source("scripts/dev/employment_weekly.R")
+
+
+
+
+# LOAD -----------------------------------------------------
+source("scripts/run_load_process.R")
