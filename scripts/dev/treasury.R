f14151f3123aa47de6a5c76db4c834e9d4b1e16f
Maya Gorbatovsky <maya.gorbatovsky@stats.govt.nz>
2021-05-20 08:37:14 +1200

    Treasury script - draft

---
+++ scripts/dev/treasury.R
@@ -0,0 +1,56 @@
+
+
+
+indicators <- list(
+  twi = list(
+    name = "Trade weighted index",
+    file = "COVID-19 - Treasury - Trade weighted index.csv"
+  ),
+  bank_bill = list(
+    name = "Bank bill yields",
+    file = "COVID-19 - Treasury - Bank bill yields.csv"
+  ),
+  foreign_exchange = list(
+    name = "Foreign exchange",
+    file = "COVID-19 - Treasury - Foreign exchange.csv"
+  ),
+  baltic_dry = list(
+    name = ""
+  ),
+  commodity_prices = list(
+    name = "Commodity prices",
+    file = "COVID-19 - Treasury - Commodity prices.csv"
+  ),
+  market_volatility = list(
+    name = "Market volatility index",
+    file = "COVID-19 - Treasury - Market volatility index.csv"
+  ),
+  global_stock = list(
+    name = "Global stock markets",
+    file = "COVID-19 - Treasury - Global stock markets.csv"
+  ),
+  nz_stock = list(
+    name = "New Zealand stock exchange",
+    file = "COVID-19 - Treasury - NZ stock exchange.csv"
+  ),
+  global_dairy = list(
+    name = "Global dairy trade",
+    file = "COVID-19 - Treasury - GLobal dairy trade.csv"
+  ),
+  property_sales = list(
+    name = "Property sales - China",
+    file = "COVID-19 - Treasury - Property sales - China.csv"
+  ),
+  transport_congestion = list(
+    name = "Transport congestion - China",
+    file = "COVID-19 - Treasury - Transport congestion - China.csv"
+  ),
+  china_purchasing = list(
+    name = "China purchasing managers index",
+    file = "COVID-19 - Treasury - China purchasing managers index.csv"
+  ),
+  investor_sentiment = list(
+    name = "Investor sentiment",
+    file = "COVID-19 - Treasury - Investor sentiment.csv"
+  )
+)
